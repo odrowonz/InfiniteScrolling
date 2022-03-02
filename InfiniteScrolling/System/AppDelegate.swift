85cea1c0edd661e36cb2e7b1d233e07b9b044647
Антипов Андрей <odrowonz@gmail.com>
2022-03-02 15:59:13 +0300

    Понижена минимальная версия iOS до 11.

--- InfiniteScrolling/System/AppDelegate.swift
+++ InfiniteScrolling/System/AppDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreData
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
@@ -46,39 +46,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 13, *) {
-            // do only pure app launch stuff, not interface stuff
+        
+        self.window = UIWindow()
+        let vm = FlickrViewModel(context: persistentContainer.viewContext)
+        let vc: FeedViewController
+        if UIDevice.current.orientation.isLandscape {
+            vc = FeedViewController(maxCountOfItemsInSection: MaxCountOfItemsInSection.horizontal.rawValue, viewmodel: vm)
         } else {
-            self.window = UIWindow()
-            window?.overrideUserInterfaceStyle = .light
-            let vm = FlickrViewModel(context: persistentContainer.viewContext)
-            let vc: FeedViewController
-            if UIDevice.current.orientation.isLandscape {
-                vc = FeedViewController(maxCountOfItemsInSection: MaxCountOfItemsInSection.horizontal.rawValue, viewmodel: vm)
-            } else {
-                vc = FeedViewController(maxCountOfItemsInSection: MaxCountOfItemsInSection.vertical.rawValue, viewmodel: vm)
-            }
-            navController.pushViewController(vc, animated: true)
-            self.window?.rootViewController = navController
-            self.window?.makeKeyAndVisible()
+            vc = FeedViewController(maxCountOfItemsInSection: MaxCountOfItemsInSection.vertical.rawValue, viewmodel: vm)
         }
+        navController.pushViewController(vc, animated: true)
+        self.window?.rootViewController = navController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
